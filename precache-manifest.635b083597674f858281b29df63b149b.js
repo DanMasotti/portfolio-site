d6def9f19ecf340e6d91891bdbad7de2761e07fa
Daniel M <danielmasotti@gmail.com>
2020-08-21 19:55:41 -0400

    Updates

---
+++ precache-manifest.635b083597674f858281b29df63b149b.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "935b8c7f012ddfd20505b1deb6e8fda3",
+    "url": "/portfolio-site/index.html"
+  },
+  {
+    "revision": "0f456c8bfcebdfab486e",
+    "url": "/portfolio-site/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "1b267b9e5949a573c9ea",
+    "url": "/portfolio-site/static/css/main.29b9ba6d.chunk.css"
+  },
+  {
+    "revision": "0f456c8bfcebdfab486e",
+    "url": "/portfolio-site/static/js/2.f14dbf66.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/portfolio-site/static/js/2.f14dbf66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b267b9e5949a573c9ea",
+    "url": "/portfolio-site/static/js/main.2dd53a15.chunk.js"
+  },
+  {
+    "revision": "a2a773adb23ff47de08a",
+    "url": "/portfolio-site/static/js/runtime-main.2d95e7a6.js"
+  },
+  {
+    "revision": "86294425bd75a18000154734764e3288",
+    "url": "/portfolio-site/static/media/dining_pic.86294425.jpg"
+  },
+  {
+    "revision": "eb12055ed65b4f3222a4b6a507f76ffa",
+    "url": "/portfolio-site/static/media/gmres_out_anim.eb12055e.gif"
+  },
+  {
+    "revision": "185e127b0ebbf61f47e616f1a2e1f47d",
+    "url": "/portfolio-site/static/media/mcmc.185e127b.gif"
+  },
+  {
+    "revision": "55dba31a970cbee647b5bc6230004371",
+    "url": "/portfolio-site/static/media/professional_pic.55dba31a.jpeg"
+  },
+  {
+    "revision": "4aa87dd15ec895ed8c950d91434ec2c3",
+    "url": "/portfolio-site/static/media/vamp.4aa87dd1.gif"
+  }
+]);
