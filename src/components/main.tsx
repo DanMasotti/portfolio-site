1b5c066c5fe86f2eee6f6c3e496fc16467903ee3
Daniel M <danielmasotti@gmail.com>
2020-08-21 20:01:50 -0400

    fixed routing

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -9,9 +9,10 @@ export const Main: React.FC = (): JSX.Element => {
   return (
     <Switch>
       <Route exact path="/" component={LandingPage} />
-      <Route path="/aboutme" component={About} />
-      <Route path="/projects" component={Projects} />
-      <Route path="/resume" component={Resume} />
+      <Route exact path="/aboutme" component={About} />
+      <Route exact path="/projects" component={Projects} />
+      <Route exact path="/resume" component={Resume} />
+      <Route path="/" component={LandingPage} />
     </Switch>
   );
 };
